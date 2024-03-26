44a286aa73b10282e49d6105477d46c744aa7acb
Otavio <oaalves@outlook.com.br>
2024-03-26 08:38:29 -0300

    Adicionado Try-on para exceções

--- bin/bank_vision.dart
+++ bin/bank_vision.dart
@@ -2,27 +2,35 @@ import 'controllers/bank_controller.dart';
 import 'models/account.dart';
 
 void main() {
+  try {
+    BankController bankController = BankController(); // Pilha 1
+    int cconta = bankController.gettingContaCorrente();
 
-  BankController bankController = BankController(); // Pilha 1
-  int cconta = bankController.gettingContaCorrente();
+    bankController.addAccount(
+        // pilha 2
+        id: 123456,
+        account: Account(
+            name: 'Otavio de Andrade Alves',
+            balance: 1000,
+            idAuthentic: true,
+            ccorrente: cconta));
 
-  bankController.addAccount( // pinha 2 
-      id: 123456,
-      account: Account(
-          name: 'Otavio de Andrade Alves',
-          balance: 1000,
-          idAuthentic: true,
-          ccorrente: cconta));
+    bankController.addAccount(
+        // pilha 3
+        id: 987654,
+        account: Account(
+            name: 'Leandro Nojento',
+            balance: 1000,
+            idAuthentic: true,
+            ccorrente: cconta));
 
-  bankController.addAccount( // pinha 3 
-      id: 987654,
-      account: Account(
-          name: 'Leandro Nojento',
-          balance: 1000,
-          idAuthentic: true,
-          ccorrente: cconta));
-
-        bool result = bankController.makeTransfer( // pinha 4 
-            idSender: 123456, idReceiver: 987654, amount: 300);
-        print(result);
+    bool result = bankController.makeTransfer(
+        // pilha 4
+        idSender: 123456,
+        idReceiver: 987654,
+        amount: 300);
+    print(result);
+  } on OutOfMemoryError {
+    print('Error! Insufficient Memory');
+  }
 }
